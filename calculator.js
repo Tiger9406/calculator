1619946d23297241b0e5878f2074880ac0f19fed
Tiger <xcao2005@gmail.com>
2023-11-23 16:23:50 -0500

    set up js event handler for clicking on button

--- calculator.js
+++ calculator.js
@@ -10,3 +10,25 @@ let current=0;
 let previous=null;
 let operation=null;
 
+function handleClick(event){
+    if(event.target.classList.contains('number')){
+        // if clicked number
+    }
+    else if(event.target.classList.contains('operator')){
+        // if clicked operator
+    }
+    else if(event.target.id=="equals"){
+        // if clicked number
+    }
+    else if(event.target.id=="dot"){
+        // if clicked number
+    }
+    else if(event.target.id=="a-clear"){
+        // if clicked all-clear
+    }
+    else if(event.target.id=="clear"){
+        // if clicked clear
+    }
+}
+
+document.querySelector("#input").addEventListener("click", handleClick);
