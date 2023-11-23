5a880723050e41bac40e09842a8ba166403c372b
Tiger <xcao2005@gmail.com>
2023-11-23 16:51:08 -0500

    edit css for better display screen, install a_clear and clear and num functions in js

--- calculator.js
+++ calculator.js
@@ -1,18 +1,32 @@
-function a_clear(){
+function num(val){
+    current+=val;
+    current_display.textContent=current;
+}
 
+function a_clear(){
+    previous = null;
+    current="0";
+    current_display.textContent=current;
+    previous_display.textContent=previous;
 }
 
 function clear(){
-
+    current = "0";
+    current_display.textContent=current;
 }
 
-let current=0;
-let previous=null;
+let current="5";
+let previous="100";
 let operation=null;
+
+const current_display=document.querySelector("#current");
+current_display.textContent=current;
+const previous_display=document.querySelector("#previous");
+previous_display.textContent=previous;
 
 function handleClick(event){
     if(event.target.classList.contains('number')){
-        // if clicked number
+        num(event.target.textContent);
     }
     else if(event.target.classList.contains('operator')){
         // if clicked operator
@@ -20,14 +34,11 @@ function handleClick(event){
     else if(event.target.id=="equals"){
         // if clicked number
     }
-    else if(event.target.id=="dot"){
-        // if clicked number
-    }
     else if(event.target.id=="a-clear"){
-        // if clicked all-clear
+        a_clear();
     }
     else if(event.target.id=="clear"){
-        // if clicked clear
+        clear();
     }
 }
 
