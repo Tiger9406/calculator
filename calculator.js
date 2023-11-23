8d79e86c80f7529809fcc257e9ec12377f310226
Tiger <xcao2005@gmail.com>
2023-11-23 15:18:00 -0500

    edit display of calculator: edit css and html input section

---
+++ calculator.js
@@ -0,0 +1,29 @@
+function addition(a, b){
+    return a+b;
+}
+
+function subtraction(a, b){
+    return a-b;
+}
+
+function multiplication(a, b){
+    return a*b;
+}
+
+function division(a, b){
+    return a/b;
+}
+
+function exponential(a, b){
+    return a**b;
+}
+
+function a_clear(){
+
+}
+
+function clear(){
+
+}
+
+let current
