fa266d9fb71798001b44aa5a5469ccdf18fa1922
Tiger <xcao2005@gmail.com>
2023-11-23 19:39:39 -0500

    include operations

--- calculator.js
+++ calculator.js
@@ -1,38 +1,82 @@
 function num(val){
-    current+=val;
-    current_display.textContent=current;
+    if(current!="0"){
+        current+=val;
+    }
+    else{
+        current=val;
+    }
+    update();
+}
+function oper(op){
+    operation=op;
+    previous=current;
+    current=0;
+    update();
+}
+
+function equals(){
+    if(operation==null){
+        return;
+    }
+    else if(operation=="+"){
+        let temp=previous;
+        previous=current;
+        current=String(Number(temp)+Number(current));
+    }
+    else if(operation=="-"){
+        let temp=previous;
+        previous=current;
+        current=String(Number(temp)-Number(current));
+    }
+    else if(operation=="*"){
+        let temp=previous;
+        previous=current;
+        current=String(Number(temp)*Number(current));
+    }
+    else if(operation=="/"){
+        let temp=previous;
+        previous=current;
+        current=String(Number(temp)/Number(current));
+    }
+    update();
+    operation=null;
 }
 
 function a_clear(){
     previous = null;
     current="0";
-    current_display.textContent=current;
-    previous_display.textContent=previous;
+    update();
 }
 
 function clear(){
     current = "0";
+    update();
+}
+
+function update(){
     current_display.textContent=current;
+    previous_display.textContent=previous;
 }
 
 let current="5";
-let previous="100";
+let previous=null;
 let operation=null;
 
 const current_display=document.querySelector("#current");
-current_display.textContent=current;
 const previous_display=document.querySelector("#previous");
-previous_display.textContent=previous;
+
+update();
 
 function handleClick(event){
     if(event.target.classList.contains('number')){
         num(event.target.textContent);
     }
-    else if(event.target.classList.contains('operator')){
-        // if clicked operator
+    else if(event.target.classList.contains('operation')){
+        console.log("hi");
+        oper(event.target.textContent);
     }
     else if(event.target.id=="equals"){
-        // if clicked number
+        equals();
     }
     else if(event.target.id=="a-clear"){
         a_clear();
